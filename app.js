9e329779a7a2857b3e1833cc5bcd8ba34a196780
halamadridmos2022-a11y <halamadridmos2022@gmail.com>
2025-12-24 19:33:14 -0500

    Add Firebase configuration to app.js

---
+++ app.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyALvD7w_dg3wigELAclTNRn_eYMwNs-U4s",
+  authDomain: "impostor-online-11805.firebaseapp.com",
+  projectId: "impostor-online-11805",
+  storageBucket: "impostor-online-11805.firebasestorage.app",
+  messagingSenderId: "409319191950",
+  appId: "1:409319191950:web:dcaa567c51cda10b61a0df",
+  measurementId: "G-G13ZR3HMGR"
+};
